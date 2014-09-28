acfdb36eddd3624aad4b214c11bb3283c6cccbb9
raccoonyy <raccoonyy@gmail.com>
2014-09-28 19:38:33 +0900

    지도 중심에 삼출 건물 표시

--- catdadmom/static/js/index.js
+++ catdadmom/static/js/index.js
@@ -1,6 +1,6 @@
 var container = document.getElementById('map'); //지도를 담을 영역의 DOM 레퍼런스
 var options = { //지도를 생성할 때 필요한 기본 옵션
-    center: new daum.maps.LatLng(33.450701, 126.570667), //지도의 중심좌표.
+    center: new daum.maps.LatLng(37.485659, 127.004036), //지도의 중심좌표.
     level: 3 //지도의 레벨(확대, 축소 정도)
 };
 
