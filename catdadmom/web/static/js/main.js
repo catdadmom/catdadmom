fd656fd7f28f65c25830d68401ea6778b62a7fad
J2P <jjp5023@gmail.com>
2014-10-16 23:11:14 +0900

    s3 upload 구현

--- catdadmom/web/static/js/main.js
+++ catdadmom/web/static/js/main.js
@@ -1,7 +1,31 @@
-var container = document.getElementById('map'); //지도를 담을 영역의 DOM 레퍼런스
-var options = { //지도를 생성할 때 필요한 기본 옵션
-    center: new daum.maps.LatLng(37.485659, 127.004036), //지도의 중심좌표.
-    level: 3 //지도의 레벨(확대, 축소 정도)
-};
+//var container = document.getElementById('map'); //지도를 담을 영역의 DOM 레퍼런스
+//var options = { //지도를 생성할 때 필요한 기본 옵션
+//    center: new daum.maps.LatLng(37.485659, 127.004036), //지도의 중심좌표.
+//    level: 3 //지도의 레벨(확대, 축소 정도)
+//};
+//
+//var map = new daum.maps.Map(container, options); //지도 생성 및 객체 리턴
 
-var map = new daum.maps.Map(container, options); //지도 생성 및 객체 리턴
+
+$(function () {
+    function s3_upload() {
+        var $status = $('#status');
+        var $preview = $('#preview');
+
+        new S3Upload({
+            file_dom_selector: 'files',
+            s3_sign_put_url: '/sign_s3/',
+            onProgress: function (percent, message) {
+                $status.text('Upload progress: ' + percent + '% ' + message);
+            },
+            onFinishS3Put: function (url) {
+                $preview.html('<img src="' + url + '">');
+            },
+            onError: function (status) {
+                $status.text('Upload error: ' + status);
+            }
+        });
+    }
+
+    $('#files').on('change', s3_upload);
+});
--- catdadmom/web/user.py
+++ catdadmom/web/user.py
@@ -2,11 +2,13 @@
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 """
+import time, os, json, base64, hmac, urllib
 import bcrypt
 from flask import (Blueprint, abort, flash, redirect, g, render_template,
-                   request, url_for, session as secure_cookie)
+                   request, url_for, Response, session as secure_cookie)
 from werkzeug.local import LocalProxy
 from sqlalchemy.exc import IntegrityError
+from hashlib import sha1
 
 from ..db import session
 from .models.user import User
@@ -55,6 +57,47 @@ def inject_current_user():
 def home():
     return render_template('home.html')
 
+@bp.route('/sign_s3/')
+def sign_s3():
+
+    if not current_user:
+        abort(403)
+
+    # Load necessary information into the application:
+    AWS_ACCESS_KEY = os.environ.get('AWS_ACCESS_KEY_ID')
+    AWS_SECRET_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+    S3_BUCKET = os.environ.get('S3_BUCKET')
+
+    # Collect information on the file from the GET parameters of the request:
+    object_name = urllib.quote_plus(request.args.get('s3_object_name'))
+    mime_type = request.args.get('s3_object_type')
+    ext = mime_type
+    ext = ext.replace('image/','')
+    object_name = str(time.time()).replace(".", "") + '.' + ext
+
+    # Set the expiry time of the signature (in seconds) and declare the permissions of the file to be uploaded
+    expires = int(time.time()+10)
+    amz_headers = "x-amz-acl:public-read"
+
+    # Generate the PUT request that JavaScript will use:
+    put_request = "PUT\n\n%s\n%d\n%s\n/%s/%s" % (mime_type, expires, amz_headers, S3_BUCKET, object_name)
+
+    # Generate the signature with which the request can be signed:
+    signature = base64.encodestring(hmac.new(AWS_SECRET_KEY, put_request, sha1).digest())
+    # Remove surrounding whitespace and quote special characters:
+    signature = urllib.quote_plus(signature.strip())
+
+    # Build the URL of the file in anticipation of its imminent upload:
+    url = 'https://%s.s3.amazonaws.com/%s' % (S3_BUCKET, object_name)
+
+    content = json.dumps({
+        'signed_request': '%s?AWSAccessKeyId=%s&Expires=%d&Signature=%s' % (url, AWS_ACCESS_KEY, expires, signature),
+        'url': url
+    })
+
+    # Return the signed request and the anticipated URL back to the browser in JSON format:
+    return Response(content, mimetype='text/plain; charset=x-user-defined')
+
 
 @bp.route('/', methods=['POST'])
 def signup():
@@ -80,7 +123,7 @@ def signup():
 def signup_form():
     """Sign up form."""
     if current_user:
-        abort(403)
+        return redirect(url_for('user.home'))
     return render_template('signup.html')
 
 
@@ -88,7 +131,7 @@ def signup_form():
 def login_form():
     """Login form."""
     if current_user:
-        abort(403)
+        return redirect(url_for('user.home'))
     return render_template('login.html')
 
 
@@ -96,7 +139,7 @@ def login_form():
 def login():
     """Logs in the user."""
     if current_user:
-        abort(403)
+        return redirect(url_for('user.home'))
     query = session.query(User).filter((User.login == request.form['login']) |
                                        (User.email == request.form['login']))
     user = query.first()
@@ -116,4 +159,4 @@ def login():
 @bp.route('/logout/')
 def logout():
     set_current_user(None)
-    return redirect(url_for('user.home'))
+    return redirect(url_for('user.login'))
